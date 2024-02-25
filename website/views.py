ece2c014ac2a71001532b8450e692feaf255ad21
asifjalaldev <jalalasif92211@gmail.com>
2024-02-25 15:53:43 +0500

    authentication and authorization

--- website/views.py
+++ website/views.py
@@ -1,13 +1,14 @@
 
 from flask import Blueprint, render_template
+from flask_login import login_user, login_required, logout_user, current_user
 
 # it has urls  so we set blueprint
 
 views = Blueprint('views',__name__)
 # register this view in __init__
-
 @views.route('/')
+@login_required
 def home():
-    return render_template('home.html', text='some text from view')
+    return render_template('home.html', user=current_user)
 
 
